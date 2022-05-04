3751191f7b9a9f7dc56b0bdf8d9a84a8b38c9aed
MarcosVel <marcos.veloso@sempreceub.com>
2022-05-03 22:53:18 -0300

    returning name of user

--- backend/src/services/user/CreateUserService.ts
+++ backend/src/services/user/CreateUserService.ts
@@ -1,6 +1,14 @@
+interface UserRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
 class CreateUserService {
-  async execute() {
-    return { ok: true };
+  async execute({ name, email, password }: UserRequest) {
+    console.log(name);
+
+    return { name: name };
   }
 }
 
