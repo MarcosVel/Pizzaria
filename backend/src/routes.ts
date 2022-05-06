c8f0b902afe36043c75e9af3e29b61be9edffb63
MarcosVel <marcos.veloso@sempreceub.com>
2022-05-05 23:54:20 -0300

    ok from /category

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -5,11 +5,16 @@ import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
 
 import { isAuthenticated } from "./middlewares/isAuthenticated";
+import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
 
 const router = Router();
 
+// user routes
 router.post("/users", new CreateUserController().handle);
 router.post("/session", new AuthUserController().handle);
 router.get("/user", isAuthenticated, new DetailUserController().handle);
+
+// category routes
+router.post("/category", isAuthenticated, new CreateCategoryController().handle);
 
 export { router };
