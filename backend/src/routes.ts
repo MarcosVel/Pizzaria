1c8c3fcd5ee25041d8f2d4a26a5c97939d550de6
MarcosVel <marcos.veloso@sempreceub.com>
2022-05-06 00:42:14 -0300

    ok from products

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -7,6 +7,7 @@ import { DetailUserController } from "./controllers/user/DetailUserController";
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
 import { ListCategoryController } from "./controllers/category/ListCategoryController";
+import { CreateProductController } from "./controllers/product/CreateProductController";
 
 const router = Router();
 
@@ -18,5 +19,8 @@ router.get("/user", isAuthenticated, new DetailUserController().handle);
 // category routes
 router.post("/category", isAuthenticated, new CreateCategoryController().handle);
 router.get("/category", isAuthenticated, new ListCategoryController().handle);
+
+// product routes
+router.post("/product", isAuthenticated, new CreateProductController().handle);
 
 export { router };
---
+++ backend/src/services/products/CreateProductService.ts
@@ -0,0 +1,23 @@
+import prismaClient from "../../prisma";
+
+interface ProductRequest {
+  name: string;
+  description: string;
+  price: number;
+  banner: string;
+  category_id: string;
+}
+
+class CreateProductService {
+  async execute({
+    name,
+    description,
+    price,
+    banner,
+    category_id,
+  }: ProductRequest) {
+    return { ok: true };
+  }
+}
+
+export { CreateProductService };
