37c9f54699f557cde0945a4842700194afc2fe5a
MarcosVel <marcos.veloso@sempreceub.com>
2022-05-10 00:15:04 -0300

    creating input component extending all props

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,4 +1,8 @@
 import Head from "next/head";
+import Image from "next/image";
+import LogoImg from "../../public/logo.svg";
+import styles from "../../styles/home.module.scss";
+import { Input } from "../components/ui/input";
 
 export default function Home() {
   return (
@@ -6,8 +10,22 @@ export default function Home() {
       <Head>
         <title>SujeitoPizza - Login</title>
       </Head>
-      <div>
-        <h1>Sujeito Pizzaria</h1>
+      <div className={styles.containerCenter}>
+        <Image src={LogoImg} alt="Logo SujeitoPizzaria" />
+
+        <div className={styles.login}>
+          <form>
+            <Input 
+              placeholder="Digite seu e-mail"
+              type="text"
+            />
+
+            <Input 
+              placeholder="Sua senha"
+              type="password"
+            />
+          </form>
+        </div>
       </div>
     </>
   );
