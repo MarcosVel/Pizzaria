55e10f0d8dcbb5d0767d5f60a944499d44a9fd56
MarcosVel <marcos.veloso@sempreceub.com>
2022-05-11 00:39:51 -0300

    register view styled

--- frontend/src/pages/signup/index.tsx
+++ frontend/src/pages/signup/index.tsx
@@ -1,32 +1,36 @@
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
-import LogoImg from "../../public/logo.svg";
-import styles from "../../styles/home.module.scss";
-import { Button } from "../components/ui/button";
-import { Input } from "../components/ui/input";
+import { Button } from "../../components/ui/button";
+import { Input } from "../../components/ui/input";
+import LogoImg from "../../../public/logo.svg";
+import styles from "../../../styles/home.module.scss";
 
-export default function Home() {
+export default function SignUp() {
   return (
     <>
       <Head>
-        <title>SujeitoPizza - Login</title>
+        <title>SujeitoPizza - Cadastro</title>
       </Head>
       <div className={styles.containerCenter}>
         <Image src={LogoImg} alt="Logo SujeitoPizzaria" />
 
         <div className={styles.login}>
+          <h1>Crie sua conta</h1>
+
           <form>
-            <Input placeholder="Digite seu e-mail" type="text" />
+            <Input placeholder="Digite seu nome" type="text" />
+            
+            <Input placeholder="Digite seu e-mail" type="email" />
 
             <Input placeholder="Sua senha" type="password" />
             <Button type="submit" loading={false}>
-              Acessar
+              Cadastrar
             </Button>
           </form>
 
           <span className={styles.register}>
-            Não possui uma conta? <Link href="/signup">Cadastre-se</Link>
+            Já possui uma conta? <Link href="/">Entrar</Link>
           </span>
         </div>
       </div>
