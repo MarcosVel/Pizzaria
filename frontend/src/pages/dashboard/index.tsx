4b2ed6967b3572075936f336eab2b02e694a2915
MarcosVel <marcos.veloso@sempreceub.com>
2022-05-14 17:41:51 -0300

    header with function to signOut

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -1,13 +1,24 @@
-import { canSSRAuth } from "../../utils/canSSRAuth"
+import Head from "next/head";
+import { Header } from "../../components/Header";
+import { canSSRAuth } from "../../utils/canSSRAuth";
 
 export default function Dashboard() {
   return (
-    <h1>Dash</h1>
-  )
+    <>
+      <Head>
+        <title>Dashboard</title>
+      </Head>
+
+      <Header />
+      <div>
+        <h1>Painel</h1>
+      </div>
+    </>
+  );
 }
 
-export const getServerSideProps = canSSRAuth(async (ctx) => {
+export const getServerSideProps = canSSRAuth(async ctx => {
   return {
-    props: {}
-  }
-})
+    props: {},
+  };
+});
