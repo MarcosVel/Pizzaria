fb9eb8d047d4d8f8f7ca5de14e652d713c0d14d3
MarcosVel <marcos.veloso@sempreceub.com>
2022-05-15 12:36:58 -0300

    preview image on new product

--- frontend/src/pages/product/index.tsx
+++ frontend/src/pages/product/index.tsx
@@ -1,10 +1,35 @@
 import Head from "next/head";
+import Image from "next/image";
+import { ChangeEvent, useState } from "react";
+import { FiUpload } from "react-icons/fi";
 import { Header } from "../../components/Header";
 import { Input } from "../../components/ui/input";
 import { canSSRAuth } from "../../utils/canSSRAuth";
 import styles from "./styles.module.scss";
 
 export default function Product() {
+  const [imageUrl, setImageUrl] = useState("");
+  const [imageFile, setImageFile] = useState(null);
+
+  function handleFile(e: ChangeEvent<HTMLInputElement>) {
+    console.log(e.target.files);
+
+    if (!e.target.files) {
+      return;
+    }
+
+    const image = e.target.files[0];
+
+    if (!image) {
+      return;
+    }
+
+    if (image.type === "image/jpeg" || image.type === "image/png") {
+      setImageFile(image);
+      setImageUrl(URL.createObjectURL(e.target.files[0]));
+    }
+  }
+
   return (
     <>
       <Head>
@@ -16,6 +41,25 @@ export default function Product() {
         <h1>Novo Produto</h1>
 
         <form>
+          <label className={styles.labelImg}>
+            <FiUpload size={30} color="#fff" />
+            <input
+              type="file"
+              accept="image/png, image/jpeg"
+              onChange={handleFile}
+            />
+
+            {imageUrl && (
+              <Image
+                src={imageUrl}
+                alt="Produto"
+                className={styles.preview}
+                width={300}
+                height={300}
+              />
+            )}
+          </label>
+
           <select>
             <option>Bebidas</option>
             <option>Pizzas</option>
