092d2addcf2fd1ad38a2c31add544d7d1f0d6cb0
MarcosVel <marcos.veloso@sempreceub.com>
2022-05-15 23:52:44 -0300

    creating product

--- frontend/src/pages/product/index.tsx
+++ frontend/src/pages/product/index.tsx
@@ -1,7 +1,8 @@
 import Head from "next/head";
 import Image from "next/image";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import { FiUpload } from "react-icons/fi";
+import { toast } from "react-toastify";
 import { Header } from "../../components/Header";
 import { Input } from "../../components/ui/input";
 import { setupAPIClient } from "../../services/api";
@@ -22,6 +23,9 @@ export default function Product({ categoryList }: CategoryProps) {
   const [imageFile, setImageFile] = useState(null);
   const [categories, setCategories] = useState(categoryList || []);
   const [categorySelected, setCategorySelected] = useState(0);
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [description, setDescription] = useState("");
 
   function handleFile(e: ChangeEvent<HTMLInputElement>) {
     console.log(e.target.files);
@@ -47,6 +51,44 @@ export default function Product({ categoryList }: CategoryProps) {
     setCategorySelected(e.target.value);
   }
 
+  async function handleRegister(e: FormEvent) {
+    e.preventDefault();
+
+    try {
+      const data = new FormData();
+
+      if (
+        name === "" ||
+        price === "" ||
+        description === "" ||
+        imageFile === null
+      ) {
+        return toast.info("Complete todos os campos!");
+      }
+
+      data.append("name", name);
+      data.append("description", description);
+      data.append("price", price);
+      data.append("category_id", categories[categorySelected].id);
+      data.append("file", imageFile);
+
+      const apiClient = setupAPIClient();
+
+      await apiClient.post("/product", data);
+
+      toast.success(`Produto "${name}" cadastrado com sucesso!`);
+
+      setName("");
+      setDescription("");
+      setPrice("");
+      setImageUrl("");
+      setImageFile(null);
+    } catch (e) {
+      console.log(e);
+      toast.error("Ops, algo deu errado!");
+    }
+  }
+
   return (
     <>
       <Head>
@@ -57,7 +99,7 @@ export default function Product({ categoryList }: CategoryProps) {
       <main className={styles.container}>
         <h1>Novo Produto</h1>
 
-        <form>
+        <form onSubmit={handleRegister}>
           <label className={styles.labelImg}>
             <FiUpload size={30} color="#fff" />
             <input
@@ -86,15 +128,26 @@ export default function Product({ categoryList }: CategoryProps) {
           </select>
 
           <div className={styles.inputs}>
-            <Input placeholder="Nome do produto" style={{ width: "70%" }} />
+            <Input
+              placeholder="Nome do produto"
+              style={{ width: "70%" }}
+              value={name}
+              onChange={e => setName(e.target.value)}
+            />
             <Input
               type="number"
               placeholder="Preço: R$ 00,00"
               style={{ width: "30%" }}
+              value={price}
+              onChange={e => setPrice(e.target.value)}
             />
           </div>
 
-          <textarea placeholder="Descreva seu produto..." />
+          <textarea
+            placeholder="Descreva seu produto..."
+            value={description}
+            onChange={e => setDescription(e.target.value)}
+          />
 
           <button>Cadastrar</button>
         </form>
