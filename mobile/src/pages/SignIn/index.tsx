1c7fdfc4e13ef5aad2347977ef27d7c5971187b1
MarcosVel <marcos.veloso@sempreceub.com>
2022-05-21 00:47:30 -0300

    styling login view

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,9 +1,37 @@
-import { Text, View } from "react-native";
+import { Text } from "react-native";
+import {
+  Image,
+  Keyboard,
+  TextInput,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
+import { theme } from "../../styles/theme";
+import styles from "./styles";
 
 export default function SignIn() {
   return (
-    <View>
-      <Text>Login</Text>
-    </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <Image style={styles.logo} source={require("../../assets/logo.png")} />
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Digite seu e-mail"
+            placeholderTextColor={theme.colors.placeholder}
+          />
+          <TextInput
+            style={styles.input}
+            secureTextEntry={true}
+            placeholder="******"
+            placeholderTextColor={theme.colors.placeholder}
+          />
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Acessar</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
