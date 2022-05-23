6c0cc0810e89991ce7fe16c031a9fdf70d66e7d3
MarcosVel <marcos.veloso@sempreceub.com>
2022-05-22 23:58:24 -0300

    loggin user

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -1,8 +1,10 @@
 import { createContext, ReactNode, useState } from "react";
+import { api } from "../services/api";
 
 type AuthContextData = {
   user: UserProps;
   isAuthenticated: boolean;
+  signIn: (credentials: SignInProps) => Promise<void>;
 };
 
 type UserProps = {
@@ -15,6 +17,12 @@ type UserProps = {
 type AuthProviderProps = {
   children: ReactNode;
 };
+
+type SignInProps = {
+  email: string;
+  password: string;
+};
+
 export const AuthContext = createContext({} as AuthContextData);
 
 export function AuthProvider({ children }: AuthProviderProps) {
@@ -24,10 +32,31 @@ export function AuthProvider({ children }: AuthProviderProps) {
     email: "",
     token: "",
   });
+  const [loadingAuth, setLoadingAuth] = useState(false);
   const isAuthenticated = !!user.name;
 
+  async function signIn({ email, password }: SignInProps) {
+    setLoadingAuth(true);
+
+    try {
+      const response = await api.post("/session", {
+        email,
+        password,
+      });
+
+      const { id, name, token } = response.data;
+
+      setUser({ id, name, email, token });
+
+      setLoadingAuth(false);
+    } catch (err) {
+      console.log("erro ao acessar", err);
+      setLoadingAuth(false);
+    }
+  }
+
   return (
-    <AuthContext.Provider value={{ user, isAuthenticated }}>
+    <AuthContext.Provider value={{ user, isAuthenticated, signIn }}>
       {children}
     </AuthContext.Provider>
   );
--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,26 +1,28 @@
-import { useState } from "react";
-import { Alert, Text } from "react-native";
+import { useContext, useState } from "react";
 import {
-  Image,
-  Keyboard,
-  TextInput,
+  Alert, Image,
+  Keyboard, Text, TextInput,
   TouchableOpacity,
   TouchableWithoutFeedback,
-  View,
+  View
 } from "react-native";
+import { AuthContext } from "../../contexts/AuthContext";
 import { theme } from "../../styles/theme";
 import styles from "./styles";
 
 export default function SignIn() {
+  const { signIn } = useContext(AuthContext);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  function handleLogin() {
+  async function handleLogin() {
     if (email === "" || password === "") {
       return Alert.alert("Campos inválidos", "Preencha os campos vazios", [
         { text: "Entendido" },
       ]);
     }
+
+    await signIn({ email, password });
   }
 
   return (
