ed80bf73871dd899c7539d8f45ea8f8f0660afc7
MarcosVel <marcos.veloso@sempreceub.com>
2022-05-24 23:22:26 -0300

    opening table

--- mobile/src/pages/Dashboard/index.tsx
+++ mobile/src/pages/Dashboard/index.tsx
@@ -1,15 +1,34 @@
+import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { useState } from "react";
 import {
   Keyboard,
   SafeAreaView,
   Text,
   TextInput,
+  ToastAndroid,
   TouchableOpacity,
-  TouchableWithoutFeedback,
+  TouchableWithoutFeedback
 } from "react-native";
+import { StackParamsList } from "../../routes/app.routes";
 import { theme } from "../../styles/theme";
 import styles from "./styles";
 
 export default function Dashboard() {
+  const navigation = useNavigation<NativeStackNavigationProp<StackParamsList>>();
+  const [number, setNumber] = useState("");
+
+  async function openOrder() {
+    if (number === "") {
+      return ToastAndroid.show("Insira o número da mesa", ToastAndroid.SHORT);
+    }
+
+    navigation.navigate("Order", {
+      number: number,
+      order_id: '2fded6aa-2bae-412b-b54b-9cd107ca5166'
+    });
+  }
+
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
       <SafeAreaView style={styles.container}>
@@ -20,9 +39,11 @@ export default function Dashboard() {
           placeholderTextColor={theme.colors.placeholder}
           style={styles.input}
           keyboardType="number-pad"
+          value={number}
+          onChangeText={setNumber}
         />
 
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity style={styles.button} onPress={openOrder}>
           <Text style={styles.buttonText}>Abrir mesa</Text>
         </TouchableOpacity>
       </SafeAreaView>
---
+++ mobile/src/pages/Order/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import styles from "./styles";
+
+export default function Order() {
+  return (
+    <View style={styles.container}>
+      <Text>Order</Text>
+    </View>
+  );
+}
