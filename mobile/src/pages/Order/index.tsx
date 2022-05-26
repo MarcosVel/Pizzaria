b2188502eb63315804a12b592fabb1bd4a9ad412
MarcosVel <marcos.veloso@sempreceub.com>
2022-05-25 23:19:41 -0300

    styling order view

--- mobile/src/pages/Order/index.tsx
+++ mobile/src/pages/Order/index.tsx
@@ -1,5 +1,15 @@
+import { Feather } from "@expo/vector-icons";
 import { RouteProp, useRoute } from "@react-navigation/native";
-import { Text, View } from "react-native";
+import {
+  Keyboard,
+  SafeAreaView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
+import { theme } from "../../styles/theme";
 import styles from "./styles";
 
 type RouteDetailParams = {
@@ -15,9 +25,36 @@ export default function Order() {
   const route = useRoute<OrderRouteProps>();
 
   return (
-    <View style={styles.container}>
-      <Text>Order</Text>
-      <Text>{route.params.number}</Text>
-    </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.title}>Mesa: {route.params.number}</Text>
+          <TouchableOpacity>
+            <Feather name="trash-2" size={28} color={theme.colors.red900} />
+          </TouchableOpacity>
+        </View>
+
+        <TouchableOpacity style={styles.select}>
+          <Text style={{ fontSize: 17, color: theme.colors.white }}>
+            Pizzas
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.select}>
+          <Text style={{ fontSize: 17, color: theme.colors.white }}>
+            Pizza de calabreza
+          </Text>
+        </TouchableOpacity>
+
+        <View style={styles.wtdContainer}>
+          <Text style={styles.qtdText}>Quantidade</Text>
+          <TextInput
+            style={[styles.select, styles.input]}
+            placeholder="0"
+            placeholderTextColor={theme.colors.placeholder}
+            keyboardType="numeric"
+          />
+        </View>
+      </SafeAreaView>
+    </TouchableWithoutFeedback>
   );
 }
--- mobile/src/pages/Order/styles.ts
+++ mobile/src/pages/Order/styles.ts
@@ -3,6 +3,48 @@ import { theme } from "../../styles/theme";
 
 export default StyleSheet.create({
   container: {
-    
+    flex: 1,
+    backgroundColor: theme.colors.dark700,
+    padding: 16,
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginTop: 24,
+    marginBottom: 12,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: theme.colors.white,
+  },
+  select: {
+    backgroundColor: theme.colors.dark900,
+    width: "100%",
+    borderRadius: 4,
+    height: 50,
+    marginBottom: 16,
+    justifyContent: "center",
+    paddingHorizontal: 16,
+  },
+  wtdContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 16,
+  },
+  qtdText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: theme.colors.white,
+  },
+  input: {
+    flex: 1,
+    marginLeft: 32,
+    textAlign: "center",
+    fontSize: 18,
+    color: theme.colors.white,
+    marginBottom: 0,
   },
 });
