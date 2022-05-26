bd5443a5367132559ef95fd8c80233390a99fa93
MarcosVel <marcos.veloso@sempreceub.com>
2022-05-25 22:59:13 -0300

    sending data throw params

--- mobile/src/pages/Order/index.tsx
+++ mobile/src/pages/Order/index.tsx
@@ -1,10 +1,23 @@
+import { RouteProp, useRoute } from "@react-navigation/native";
 import { Text, View } from "react-native";
 import styles from "./styles";
 
+type RouteDetailParams = {
+  Order: {
+    number: string | number;
+    order_id: string;
+  };
+};
+
+type OrderRouteProps = RouteProp<RouteDetailParams, "Order">;
+
 export default function Order() {
+  const route = useRoute<OrderRouteProps>();
+
   return (
     <View style={styles.container}>
       <Text>Order</Text>
+      <Text>{route.params.number}</Text>
     </View>
   );
 }
