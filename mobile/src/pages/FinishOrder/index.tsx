ed3061a156d2ad7e131e83a4c3f9abb3e41e8e49
MarcosVel <marcos.veloso@sempreceub.com>
2022-06-04 15:32:56 -0300

    finish order

--- mobile/src/pages/FinishOrder/index.tsx
+++ mobile/src/pages/FinishOrder/index.tsx
@@ -1,6 +1,9 @@
 import { Feather } from "@expo/vector-icons";
-import { RouteProp, useRoute } from "@react-navigation/native";
-import { Text, TouchableOpacity, View } from "react-native";
+import { RouteProp, useNavigation, useRoute } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { Text, ToastAndroid, TouchableOpacity, View } from "react-native";
+import { StackParamsList } from "../../routes/app.routes";
+import { api } from "../../services/api";
 import { theme } from "../../styles/theme";
 import styles from "./styles";
 
@@ -15,19 +18,36 @@ type FinishOrderRouteProp = RouteProp<RouteDetailParams, "FinishOrder">;
 
 export default function FinishOrder() {
   const route = useRoute<FinishOrderRouteProp>();
+  const navigation =
+    useNavigation<NativeStackNavigationProp<StackParamsList>>();
 
   async function handleFinish() {
-    alert("Finish order")
+    try {
+      await api.put("/order/send", {
+        order_id: route.params?.order_id,
+      });
+
+      ToastAndroid.show(
+        `Pedido realizado mesa: ${route.params?.number}`,
+        ToastAndroid.SHORT
+      );
+
+      navigation.popToTop();
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   return (
     <View style={styles.container}>
       <Text style={styles.alert}>Você deseja finalizar este pedido?</Text>
-      <Text style={styles.title}>Mesa {route.params?.number}</Text>
+      <Text style={styles.title}>Mesa: {route.params?.number}</Text>
 
       <TouchableOpacity style={styles.button}>
         <Feather name="shopping-cart" size={20} color={theme.colors.dark900} />
-        <Text style={styles.textButton} onPress={handleFinish}>Finalizar pedido</Text>
+        <Text style={styles.textButton} onPress={handleFinish}>
+          Finalizar pedido
+        </Text>
       </TouchableOpacity>
     </View>
   );
