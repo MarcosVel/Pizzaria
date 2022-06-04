949782571f6c15e86c8ed066b3b2b1a1ab9cf737
MarcosVel <marcos.veloso@sempreceub.com>
2022-06-04 12:44:29 -0300

    getting params of order to finish

--- mobile/src/pages/FinishOrder/index.tsx
+++ mobile/src/pages/FinishOrder/index.tsx
@@ -1,17 +1,33 @@
 import { Feather } from "@expo/vector-icons";
+import { RouteProp, useRoute } from "@react-navigation/native";
 import { Text, TouchableOpacity, View } from "react-native";
 import { theme } from "../../styles/theme";
 import styles from "./styles";
 
+type RouteDetailParams = {
+  FinishOrder: {
+    number: string | number;
+    order_id: string;
+  };
+};
+
+type FinishOrderRouteProp = RouteProp<RouteDetailParams, "FinishOrder">;
+
 export default function FinishOrder() {
+  const route = useRoute<FinishOrderRouteProp>();
+
+  async function handleFinish() {
+    alert("Finish order")
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.alert}>Você deseja finalizar este pedido?</Text>
-      <Text style={styles.title}>Mesa 30</Text>
+      <Text style={styles.title}>Mesa {route.params?.number}</Text>
 
       <TouchableOpacity style={styles.button}>
         <Feather name="shopping-cart" size={20} color={theme.colors.dark900} />
-        <Text style={styles.textButton}>Finalizar pedido</Text>
+        <Text style={styles.textButton} onPress={handleFinish}>Finalizar pedido</Text>
       </TouchableOpacity>
     </View>
   );
